6917b0d85fbf43bbeaf54ddeaa4bd3de617ba86b
Domagoj <paulic.domagoj@gmail.com>
2025-08-25 10:35:34 +0200

    enhance document loading by adding text splitting functionality

--- main.py
+++ main.py
@@ -1,9 +1,20 @@
 from langchain.document_loaders import TextLoader
+from langchain.text_splitter import CharacterTextSplitter
 from dotenv import load_dotenv
 
 load_dotenv()
 
+text_splitter = CharacterTextSplitter(
+    separator="\n",
+    chunk_size=200,
+    chunk_overlap=0
+)
+
 loader = TextLoader("facts.txt")
-docs = loader.load()
+docs = loader.load_and_split(text_splitter)
 
 print(docs)
+
+for doc in docs:
+    print(doc.page_content)
+    print("\n")
