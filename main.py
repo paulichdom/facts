cb96d207dbd9c85337731d5049e8eade648e9110
Domagoj <paulic.domagoj@gmail.com>
2025-08-25 11:09:06 +0200

    integrate embeddings and database functionality for document similarity search

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 from langchain.document_loaders import TextLoader
 from langchain.text_splitter import CharacterTextSplitter
+from langchain_openai import OpenAIEmbeddings
+from langchain.vectorstores.chroma import Chroma
 from dotenv import load_dotenv
 
 load_dotenv()
+
+embeddings = OpenAIEmbeddings()
 
 text_splitter = CharacterTextSplitter(
     separator="\n",
@@ -13,8 +17,18 @@ text_splitter = CharacterTextSplitter(
 loader = TextLoader("facts.txt")
 docs = loader.load_and_split(text_splitter)
 
-print(docs)
+db = Chroma.from_documents(
+    docs,
+    embedding=embeddings,
+    persist_directory="emb"
+)
 
-for doc in docs:
-    print(doc.page_content)
+results = db.similarity_search(
+    "What is an interesting fact about the English language?",
+)
+
+print("Results:", results)
+
+for result in results:
     print("\n")
+    print(result.page_content)
